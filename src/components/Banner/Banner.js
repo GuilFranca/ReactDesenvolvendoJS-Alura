8a26f727532d2e2f71996697c88150a081537886
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-10-01 15:50:23 -0300

    component banner inserido na aplicação

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    // JSX -> Parece HTML mas não é
+    return (
+        <header className='banner'>
+            <img src="/images/banner.png" alt="O banner principal da página Organa"></img>
+        </header>
+    )
+}
+
+export default Banner;
