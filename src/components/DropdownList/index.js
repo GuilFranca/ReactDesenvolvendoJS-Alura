14a071144a0e1d1850c4783db6a107178346dc6d
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-10-16 22:12:00 -0300

    Aula 2 - 03 Lista dos times

---
+++ src/components/DropdownList/index.js
@@ -0,0 +1,21 @@
+import './DropdownList.css';
+
+const DropdownList = (props) => {
+
+    console.log(props.items);
+
+    return (
+        <div className='dropdown__list'>
+            <label>{props.label}</label>
+            <select>
+                {/* {props.items.map(item => <option>{item}</option>)} */}
+                {props.items.map((item) => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+
+}
+
+export default DropdownList;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,18 @@
 import './Form.css';
 import TextField from '../TextField';
+import DropdownList from '../DropdownList';
 
 const Form = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
 
     return (
 
@@ -11,6 +22,7 @@ const Form = () => {
                 <TextField label="Nome" placeholder="Digite seu nome" />
                 <TextField label="Cargo" placeholder="Digite seu cargo" />
                 <TextField label="Imagem" placeholder="Informe o endereço da imagem" />
+                <DropdownList label='Time' items={times}/>
             </form>
         </section>
 
